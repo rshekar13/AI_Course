c3f74307d5cbb16b8558c1b6a38ea4b1fac0e8a1
rshekar13 <105404888+rshekar13@users.noreply.github.com>
2023-01-31 00:25:54 +0530

    Add files via upload

--- model_deployment/app.py
+++ model_deployment/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+#from  waitress import serve
 import pandas as pd
 import pickle
 import joblib
@@ -13,13 +14,13 @@ def preprocess():
     """ function to preprocess the given input to the data required for model"""
    
     req = request.form.to_dict()
-    col = pd.read_csv(r'col_names.csv').columns
-    drop_col = pd.read_csv(r'drop_col.csv').columns
+    col = pd.read_csv(r'model_deployment\col_names.csv').columns
+    drop_col = pd.read_csv(r'model_deployment\drop_col.csv').columns
     drop_col = [i.strip() for i in drop_col]
     cat_list = ['X0', 'X1', 'X2', 'X3', 'X5', 'X6', 'X8']
     query=  pd.read_csv(io.StringIO(req['review_text']), sep=",",names =col)
     query.drop(drop_col,axis=1,inplace=True)
-    encode= joblib.load('cat_encode1.pkl')
+    encode= joblib.load('model_deployment\cat_encode1.pkl')
     qvery_encd = encode.transform(query[cat_list])
     qvery_encd  =pd.DataFrame(qvery_encd,columns=encode.get_feature_names()) 
     kbest_indices= [  0,   1,   4,   7,   8,   9,  11,  14,  15,  17,  19,  22,  25,
@@ -38,12 +39,10 @@ def preprocess():
 
 
 
+
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/index')
 def index():
     return flask.render_template('index.html')
 
@@ -51,7 +50,7 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     final_df = preprocess()
-    model = joblib.load("xbg_best.pkl")
+    model = joblib.load(r"model_deployment\xbg_best.pkl")
     ypred = float(model.predict(final_df))
     ypred = round(ypred,2)
     print(ypred)
@@ -60,4 +59,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8082,debug=True)
+    app.run(port=8082,debug=False)
+    
